1e29ab16275bc11c783adddf119fd620c69107d8
Victoria Lepeshinskaya <vica.lepeshinskaya@gmail.com>
2024-12-10 22:05:49 +0100

    WebDriverManager, configuring other browsers, WebDriver

---
+++ Selenium4Examples/src/main/java/TestBrowserExe.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestBrowserExe {
+
+
+
+	public static void main(String[] args) {
+		
+		
+		WebDriver driver = new ChromeDriver();
+
+	}
+
+}
--- Selenium4Examples/src/test/java/testCases/testBrowsers.java
+++ Selenium4Examples/src/test/java/testCases/testBrowsers.java
@@ -1,14 +1,34 @@
 package testCases;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class testBrowsers {
 
+	public static String browser = "firefox";
+	public static WebDriver driver;
+
 	public static void main(String[] args) {
-		
-		
-		WebDriverManager.chromedriver().setup();
-		ChromeDriver driver = new ChromeDriver();
+
+		if (browser.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+
+		else if (browser.equals("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		}
+
+		else if (browser.equals("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+
 		driver.get("http://way2automation.com");
 		System.out.println(driver.getTitle());
 		driver.quit();
